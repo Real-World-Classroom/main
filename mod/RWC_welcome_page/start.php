0b7bac653b9625675a140b03927b616c2422fab7
Daniel Charest <d4n131.ch4r357@gmail.com>
2014-09-03 03:43:49 -0400

    Changes to welcome page

--- mod/RWC_welcome_page/start.php
+++ mod/RWC_welcome_page/start.php
@@ -1,14 +1,28 @@
 <?php
 
+// Initialize plugin
 elgg_register_event_handler('init', 'system', 'RWC_welcome_page_init');
 
 function RWC_welcome_page_init() {
-	elgg_register_plugin_hook_handler('index', 'system', 'new_index');
+
+	// Extend system CSS with our own styles
+	elgg_extend_view('css/elgg', 'RWC_welcome_page/css');
+
+	// Replace the default index page
+	elgg_register_plugin_hook_handler('index', 'system', 'custom_index');
 }
 
-function new_index() {
-	if (!include_once(dirname(__FILE__) . "/RWC_welcome_page.php")) {
+function custom_index($hook, $type, $return, $params) {
+	if ($return == true) {
+		// Another hook has already replaced the front page
+		return $return;
+	}
+
+	// Attempt to include new index, but return false if failure
+	if (!include_once(dirname(__FILE__) . "/index.php")) {
 		return false;
 	}
+
+	// Otherwise return true to signify that we have handled the front page
 	return true;
 }
---
+++ mod/RWC_welcome_page/views/default/page/layouts/RWC_welcome_page.php
@@ -0,0 +1,87 @@
+<?php
+
+echo'
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css">
+		<script type="text/javascript" src="script.js"></script>
+	</head>
+	<body>
+
+		<div id="frame"> 
+			 <div id="header">
+			 <h1 id="welcomeHeader" class="textCentered">Welcome to the Real World Classroom!</h1>
+			 <h4 class="textCentered">Continue to the rest of the site <a href="http://54.187.111.184/realworldclassroom/activity/">here</a></h4><br>
+			 <h4 class="textCentered"><a href="http://54.187.111.184/realworldclassroom/activity/">Welcome Page Editor</a></h4>
+			 </div>
+						
+			 <div id="box1-1" class="row1 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+					
+			 <div id="box1-2" class="row1 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+						
+			 <div id="box1-3" class="row1 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+						
+			 <div id="box1-4" class="row1 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+			 <div id="box2-1" class="row2 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+							 
+			 <div id="box2-2" class="row2 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+			 <div id="box2-3" class="row2 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+						 
+			 <div id="box2-4" class="row2 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+			 <div id="box3-1" class="row3 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+			 <div id="box3-2" class="row3 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+			 <div id="box3-3" class="row3 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+			 <div id="box3-4" class="row3 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+			 <div id="box4-1" class="row4 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+			 <div id="box4-2" class="row4 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+			 <div id="box4-3" class="row4 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+			 <div id="box4-4" class="row4 gridbox">
+			 <p>Filler text for now</p>
+			 </div>
+								 
+		</div>
+						
+						
+
+	</body>
+</html>';
